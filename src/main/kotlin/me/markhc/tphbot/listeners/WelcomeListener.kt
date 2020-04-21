dc7f844d6ca770f3b5c69fe98c26f149a15d2cd9
markhc <mconceicao@ntstatus.net>
2020-04-21 20:03:26 -0300

    Added welcome embed

--- src/main/kotlin/me/markhc/tphbot/listeners/WelcomeListener.kt
+++ src/main/kotlin/me/markhc/tphbot/listeners/WelcomeListener.kt
@@ -7,10 +7,20 @@ import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent
 import me.markhc.tphbot.services.GuildConfiguration
 import me.markhc.tphbot.services.findOrCreate
 import mu.KLogger
+import net.dv8tion.jda.api.EmbedBuilder
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.awt.Color
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.TimeUnit
+import kotlin.concurrent.schedule
+
+typealias MessageID = String
+typealias UserID = String
 
 class WelcomeListener(private val logger: KLogger) {
+    private val welcomeMessages = ConcurrentHashMap<UserID, MessageID>()
+
     @Subscribe
     fun onGuildMemberJoinEvent(event: GuildMemberJoinEvent) {
         val guild = transaction {
@@ -25,15 +35,36 @@ class WelcomeListener(private val logger: KLogger) {
         logger.info { "$user created $numOfDays days ago ($date) -- joined the server" }
 
         if(guild.welcomeChannel == null) {
-            logger.error { "Welcome channel not set" }
+            logger.debug { "Welcome channel not set" }
         }
 
-        val welcomeChannel = event.guild.textChannels.find {
+        val welcomeChannel = event.guild.textChannels.find  {
             it.id == guild.welcomeChannel
         }
 
-        logger.info { "Sending welcome embed on channel ${welcomeChannel ?: "null"}" }
+        val userImage = event.user.effectiveAvatarUrl
 
-        welcomeChannel?.sendMessage("Welcome ${event.user.name}! Have a cookie.")
+        welcomeChannel?.sendMessage(buildJoinMessage(
+                title = "Welcome",
+                response = "Aww yeeee it\u0027s ${event.user.asMention}(${event.user.fullName()}",
+                image = userImage)
+        )?.queue{msg ->
+            msg.addReaction("\uD83D\uDC4B").queue {
+                welcomeMessages[event.user.id] = msg.id
+                Timer().schedule(1000 * 10) {
+                    welcomeMessages.remove(event.user.id)
+                    logger.debug { "Removed welcome message from list" }
+                }
+            }
+        }
     }
+
+    private fun buildJoinMessage(response: String, image: String, title: String) =
+            EmbedBuilder()
+                    .setTitle(title)
+                    .setDescription(response)
+                    .setColor(Color.red)
+                    .setThumbnail(image)
+                    .build()
+
 }
