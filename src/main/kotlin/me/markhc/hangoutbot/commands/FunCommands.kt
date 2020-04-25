f89831b7e716f6c535993bf33d9a4376e0a1557c
markhc <mconceicao@ntstatus.net>
2020-04-25 10:53:44 -0300

    Added cowsay

--- src/main/kotlin/me/markhc/hangoutbot/commands/FunCommands.kt
+++ src/main/kotlin/me/markhc/hangoutbot/commands/FunCommands.kt
@@ -1,12 +1,19 @@
 package me.markhc.hangoutbot.commands
 
+import com.github.ricksbrown.cowsay.Cowsay
 import me.aberrantfox.kjdautils.api.annotation.CommandSet
 import me.aberrantfox.kjdautils.api.dsl.command.commands
 import me.aberrantfox.kjdautils.internal.arguments.IntegerArg
+import me.aberrantfox.kjdautils.internal.arguments.SentenceArg
 import me.aberrantfox.kjdautils.internal.arguments.SplitterArg
+import me.aberrantfox.kjdautils.internal.arguments.WordArg
 import me.markhc.hangoutbot.locale.Messages
 
 import kotlin.random.Random
+
+object CowsayData {
+    val validCows = Cowsay.say(arrayOf("-l")).split("\n").filterNot { listOf("sodomized", "head-in", "telebears").contains(it) }
+}
 
 @CommandSet("Fun")
 fun funCommands() = commands {
@@ -59,5 +66,20 @@ fun funCommands() = commands {
 
             it.respond("$result")
         }
+    }
+
+    command("cowsay") {
+        description = "Displays a cowsay with a given message. Run with no arguments to get a list of valid cows."
+        execute(WordArg("Cow").makeOptional { "" }, SentenceArg("Message").makeOptional { "" }) {
+            val (arg0, arg1) = it.args
+
+            it.respond(when {
+                arg0.isBlank() && arg1.isBlank() -> CowsayData.validCows.joinToString (", ")
+                arg1.isBlank() -> "```${Cowsay.say(arrayOf(arg0))}```"
+                CowsayData.validCows.contains(arg0) -> "```${Cowsay.say(arrayOf("-f $arg0", arg1))}```"
+                else -> "```${Cowsay.say(arrayOf("$arg0 $arg1"))}```"
+            })
+        }
+
     }
 }
