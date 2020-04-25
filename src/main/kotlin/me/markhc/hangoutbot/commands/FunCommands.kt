cf40aa2aa28dd856d3a7abdc7c5be29513e81aaf
markhc <mconceicao@ntstatus.net>
2020-04-25 10:16:21 -0300

    Added flip

--- src/main/kotlin/me/markhc/hangoutbot/commands/FunCommands.kt
+++ src/main/kotlin/me/markhc/hangoutbot/commands/FunCommands.kt
@@ -3,11 +3,14 @@ package me.markhc.hangoutbot.commands
 import me.aberrantfox.kjdautils.api.annotation.CommandSet
 import me.aberrantfox.kjdautils.api.dsl.command.commands
 import me.aberrantfox.kjdautils.internal.arguments.IntegerArg
+import me.aberrantfox.kjdautils.internal.arguments.SplitterArg
+import me.markhc.hangoutbot.locale.Messages
+
 import kotlin.random.Random
 
 @CommandSet("Fun")
 fun funCommands() = commands {
-    command("flip") {
+    command("coin") {
         description = "Flip a coin (or coins)"
         execute(IntegerArg("Coins").makeOptional { 1 }) {
             val (coins) = it.args
@@ -39,13 +42,22 @@ fun funCommands() = commands {
         }
     }
 
+    command("flip") {
+        description = "Choose one of "
+        execute(SplitterArg("Choice 1 | Choice 2 | ...")) {
+            val (args) = it.args
+            val choice = args[Random.nextInt(args.size)]
+            it.respond(Messages.getRandomFlipMessage(choice))
+        }
+    }
+
     command("roll") {
         description = "Rolls a number in a range (default 1-100)"
-        execute(IntegerArg("Min"), IntegerArg("Max")) {
+        execute(IntegerArg("Min").makeOptional{1}, IntegerArg("Max").makeOptional { 100 }) {
             val (a, b) = it.args
             val result = if(a > b) Random.nextInt(b, a) else Random.nextInt(a, b)
 
-            it.respond("$result!")
+            it.respond("$result")
         }
     }
 }
--- src/main/kotlin/me/markhc/hangoutbot/locale/Messages.kt
+++ src/main/kotlin/me/markhc/hangoutbot/locale/Messages.kt
@@ -1,5 +1,16 @@
 package me.markhc.hangoutbot.locale
 
+import kotlin.random.Random
+
 object Messages {
     val COMMAND_NOT_SUPPORTED_IN_DMS = "This command is not supported in DMs"
+    private val FLIP_RESPONSE_CHOICES = listOf(
+            "Hmm, I'd say %choice%.",
+            "%choice%, no doubt.",
+            "Perhaps... %choice%.",
+            "%choice% sounds good to me.",
+            "If it were up to me, I'd go with %choice%"
+    )
+    fun getRandomFlipMessage(choice: String) =
+            FLIP_RESPONSE_CHOICES[Random.nextInt(FLIP_RESPONSE_CHOICES.size)].replace("%choice%", choice)
 }
