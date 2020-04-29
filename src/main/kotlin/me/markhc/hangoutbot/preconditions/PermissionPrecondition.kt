e674e32504b58395df33c5c454ca659df0aa8123
markhc <mconceicao@ntstatus.net>
2020-04-29 15:20:09 -0300

    Bot reacts with an X to unknown commands

--- src/main/kotlin/me/markhc/hangoutbot/preconditions/PermissionPrecondition.kt
+++ src/main/kotlin/me/markhc/hangoutbot/preconditions/PermissionPrecondition.kt
@@ -5,10 +5,15 @@ import me.aberrantfox.kjdautils.extensions.jda.toMember
 import me.aberrantfox.kjdautils.internal.command.*
 import me.markhc.hangoutbot.extensions.requiredPermissionLevel
 import me.markhc.hangoutbot.services.*
+import net.dv8tion.jda.api.entities.Emote
 
 @Precondition
 fun produceHasPermissionPrecondition(permissionsService: PermissionsService) = precondition {
-    val command = it.container[it.commandStruct.commandName] ?: return@precondition Fail()
+    val command = it.container[it.commandStruct.commandName]
+    if(command == null) {
+        it.message.addReaction("\u274C").queue()
+        return@precondition Fail()
+    }
     val requiredPermissionLevel = command.requiredPermissionLevel ?: DEFAULT_REQUIRED_PERMISSION
     val guild = it.guild!!
     val member = it.author.toMember(guild)!!
