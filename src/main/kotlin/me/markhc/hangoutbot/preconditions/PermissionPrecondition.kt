cfa067b762451558f1106e0db9164b3e7e7bf280
markhc <mconceicao@ntstatus.net>
2020-05-07 20:26:01 -0300

    Allow Administrators and above to issue commands from anywhere even when the bot channel is set

--- src/main/kotlin/me/markhc/hangoutbot/preconditions/PermissionPrecondition.kt
+++ src/main/kotlin/me/markhc/hangoutbot/preconditions/PermissionPrecondition.kt
@@ -4,20 +4,25 @@ import me.aberrantfox.kjdautils.api.annotation.Precondition
 import me.aberrantfox.kjdautils.internal.command.Fail
 import me.aberrantfox.kjdautils.internal.command.Pass
 import me.aberrantfox.kjdautils.internal.command.precondition
+import me.markhc.hangoutbot.services.PermissionLevel
 import me.markhc.hangoutbot.services.PermissionsService
 import me.markhc.hangoutbot.services.PersistentData
 
 @Precondition
 fun produceHasPermissionPrecondition(persistentData: PersistentData, permissionsService: PermissionsService) = precondition {
     val guild = it.guild ?: return@precondition Fail()
+    val member = it.guild!!.getMember(it.author)!!
+
     val botChannel = persistentData.getGuildProperty(guild) { botChannel }
-    if (botChannel.isNotEmpty() && it.channel.id != botChannel)
+    if (botChannel.isNotEmpty()
+            && it.channel.id != botChannel
+            && permissionsService.getPermissionLevel(member) > PermissionLevel.Administrator)
         return@precondition Fail()
 
     val command = it.container[it.commandStruct.commandName] ?: return@precondition Fail()
     val level = permissionsService.getCommandPermissionLevel(it.guild!!, command)
 
-    if (!permissionsService.hasClearance(it.guild!!, it.author, level))
+    if (!permissionsService.hasClearance(member, level))
         return@precondition Fail("You do not have the required permissions to perform this action.")
 
     return@precondition Pass
