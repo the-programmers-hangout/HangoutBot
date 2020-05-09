1874551b9cf4a89b23209a4d43fee729f8b4e2b8
markhc <mconceicao@ntstatus.net>
2020-05-09 20:30:02 -0300

    Development (#18)
    * Added listroles & deleterole commands and a role delete listener
    
    * Removed duplicate code
    
    * Now load guild members before listing roles
    
    * Fixing listroles
    
    * Removed seemingly unecessary call to retrieveMembers

--- src/main/kotlin/me/markhc/hangoutbot/commands/serverutilities/StaffUtilityCommands.kt
+++ src/main/kotlin/me/markhc/hangoutbot/commands/serverutilities/StaffUtilityCommands.kt
@@ -102,40 +102,38 @@ fun produceStaffUtilityCommands(persistentData: PersistentData,
         }
     }
 
-    val isChangingColor: MutableList<Long> = mutableListOf()
+    val commandsInExecution = mutableMapOf<Long, Boolean>()
+
     command("setcolor") {
         description = "Creates a role with the given name and color and assigns it to the user."
-        requiredPermissionLevel = PermissionLevel.Administrator
+        requiredPermissionLevel = PermissionLevel.Staff
         requiresGuild = true
-        execute(EitherArg(QuoteArg, WordArg, "RoleName"), HexColorArg("HexColor").makeNullableOptional()) { event ->
-            val (roleName, color) = event.args
+        execute(HexColorArg("HexColor").makeNullableOptional(), SentenceArg("RoleName")) { event ->
+            val (color, roleName) = event.args
 
-            val member = event.guild!!.getMember(event.author)!!
+            val guild = event.guild!!
+            val member = guild.getMember(event.author)!!
 
-            if(isChangingColor.contains(member.idLong)) {
+            if(commandsInExecution[guild.idLong] == true)
                 return@execute event.respond("Cannot do that right now")
-            }
 
-            isChangingColor.add(member.idLong)
+            commandsInExecution[guild.idLong] = true
 
-            kotlin.runCatching {
-                when (roleName) {
-                    is Either.Left -> colorService.setMemberColor(member, roleName.left, color)
-                    is Either.Right -> colorService.setMemberColor(member, roleName.right, color)
-                }
+            runCatching {
+                colorService.setMemberColor(member, roleName, color)
             }.onSuccess {
-                event.respond("Successfully assigned color")
+                event.respond("Successfully assigned color $roleName")
             }.onFailure {
                 event.respond(it.message!!)
             }
 
-            isChangingColor.remove(member.idLong)
+            commandsInExecution[guild.idLong] = false
         }
     }
 
     command("clearcolor") {
         description = "Clears the current color role."
-        requiredPermissionLevel = PermissionLevel.Administrator
+        requiredPermissionLevel = PermissionLevel.Staff
         requiresGuild = true
         execute { event ->
             val member = event.guild!!.getMember(event.author)!!
@@ -148,23 +146,23 @@ fun produceStaffUtilityCommands(persistentData: PersistentData,
 
     command("listcolors") {
         description = "Creates a role with the given name and color and assigns it to the user."
-        requiredPermissionLevel = PermissionLevel.Administrator
+        requiredPermissionLevel = PermissionLevel.Staff
         requiresGuild = true
         execute { event ->
             val colorRoles = persistentData.getGuildProperty(event.guild!!) { assignedColorRoles }
 
-            if(colorRoles.isEmpty()) {
+            if (colorRoles.isEmpty()) {
                 return@execute event.respond("No colors set. For more information, see `${event.discord.configuration.prefix}help setcolor`")
             }
 
             val colorInfo = colorRoles.map {
                 event.guild!!.getRoleById(it.key)?.asMention ?: it.key
-            }.sortedBy{ it.length } .joinToString("\n")
+            }.sortedBy { it.length }.joinToString("\n")
 
             event.respond(embed {
                 title = "Currently used colors"
                 description = "Run `setcolor <name>` to use one of the colors here.\n" +
-                              "Run `setcolor <name> <hexcolor>` to create a new color."
+                        "Run `setcolor <hexcolor> <name>` to create a new color."
                 color = infoColor
 
                 field {
@@ -172,6 +170,57 @@ fun produceStaffUtilityCommands(persistentData: PersistentData,
                     value = colorInfo
                 }
             })
+        }
+    }
+
+    command("listroles") {
+        description = "List all the roles available in the guild. Number of users might not be accurate as it only includes cached users."
+        requiredPermissionLevel = PermissionLevel.Staff
+        requiresGuild = true
+        execute { event ->
+            val guild = event.guild!!
+
+            val message = event.channel.sendMessage("Working...").complete()
+
+            val list = guild.roles.map {
+                "${it.id} - ${it.name}: ${guild.getMembersWithRoles(it).size} users"
+            }
+
+            val response = list.joinToString("\n")
+            if(response.length < 1990) {
+                message.editMessage("```\n$response\n```").queue()
+            } else {
+                var data = ""
+                var edited = false
+                for(i in 0..list.size) {
+                    if(data.length + list[i].length < 1990) {
+                        data += list[i] + '\n'
+                    } else {
+                        if(!edited) {
+                            message.editMessage("```\n$data\n```").queue()
+                            edited = true
+                        } else {
+                            event.channel.sendMessage("```\n$data\n```").queue()
+                        }
+                        data = list[i] + '\n'
+                    }
+                }
+                event.channel.sendMessage("```\n$data\n```").queue()
+            }
+        }
+    }
+
+    command("deleterole") {
+        description = "Deletes the given role or roles. HERE BE DRAGONS."
+        requiredPermissionLevel = PermissionLevel.GuildOwner
+        requiresGuild = true
+        execute(MultipleArg(RoleArg)) { event ->
+            event.args.first.forEach { role ->
+                role.delete().queue(
+                        { event.respond("Deleted role ${role.name}") },
+                        { event.respond("Failed to delete role ${role.name}") }
+                )
+            }
         }
     }
 }
@@ -193,9 +242,8 @@ private fun removeRoles(guild: Guild, member: Member, vararg roles: String) {
     }
 }
 
-private fun grantRole(guild: Guild, member: Member, role: Role) {
-    guild.addRoleToMember(member, role).queue()
-}
+private fun grantRole(guild: Guild, member: Member, role: Role) =
+        guild.addRoleToMember(member, role).queue()
 
 private fun buildRolesEmbed(guild: Guild, roles: Map<String, List<String>>): MessageEmbed {
     return embed {
