3d76fb5a9ae556d75e83524fb3d02a91a0471314
markhc <mconceicao@ntstatus.net>
2020-06-17 17:58:33 -0300

    Member chunking (#25)
    * Bumped KUtils to 0.17.2-SNAPSHOT
    
    * Enabled member chuking and all intents

--- src/main/kotlin/me/markhc/hangoutbot/Main.kt
+++ src/main/kotlin/me/markhc/hangoutbot/Main.kt
@@ -1,10 +1,14 @@
 package me.markhc.hangoutbot
 
 import com.google.gson.Gson
-import me.jakejmattson.kutils.api.dsl.configuration.startBot
+import me.jakejmattson.kutils.api.dsl.configuration.bot
 import me.markhc.hangoutbot.dataclasses.Properties
 import me.markhc.hangoutbot.dataclasses.loadConfig
+import net.dv8tion.jda.api.requests.GatewayIntent
+import net.dv8tion.jda.api.utils.ChunkingFilter
+import net.dv8tion.jda.api.utils.MemberCachePolicy
 import java.awt.Color
+import java.util.*
 
 fun main() {
     loadConfig {
@@ -13,8 +17,19 @@ fun main() {
         val properties = Gson().fromJson(propFile, Properties::class.java)
                 ?: throw Exception("Failed to parse properties")
 
-        startBot(configuration.token, enableScriptEngine = true) {
-            registerInjectionObjects(configuration, properties)
+        bot(configuration.token) {
+            client {
+                jdaBuilder.setChunkingFilter(ChunkingFilter.ALL)
+                        .enableIntents(EnumSet.allOf(GatewayIntent::class.java))
+                        .setMemberCachePolicy(MemberCachePolicy.ALL)
+            }
+
+            injection {
+                enableScriptEngineService = true
+
+                inject(configuration)
+                inject(properties)
+            }
 
             configure {
                 colors {
@@ -25,6 +40,9 @@ fun main() {
                 commandReaction = null
                 allowMentionPrefix = true
                 requiresGuild = false
+            }
+            logging {
+                generateCommandDocs = false
             }
         }
     }
