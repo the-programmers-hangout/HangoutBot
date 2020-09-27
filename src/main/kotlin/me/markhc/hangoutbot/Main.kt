bd65b9d6194e1be399ff54c412f31d483c0760d6
JakeJMattson <JakeJMattson@gmail.com>
2020-09-27 03:46:11 -0500

    Serialization changes

--- src/main/kotlin/me/markhc/hangoutbot/Main.kt
+++ src/main/kotlin/me/markhc/hangoutbot/Main.kt
@@ -4,84 +4,81 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import me.jakejmattson.discordkt.api.dsl.bot
 import me.jakejmattson.discordkt.api.extensions.addInlineField
-import me.markhc.hangoutbot.dataclasses.*
+import me.markhc.hangoutbot.dataclasses.Properties
 import me.markhc.hangoutbot.services.*
 import java.awt.Color
 
-suspend fun main() {
-    loadConfig {
-        val configuration = it ?: throw Exception("Failed to parse configuration")
-        val propFile = Properties::class.java.getResource("/hangoutbot_properties.json").readText()
-        val properties = Json.decodeFromString<Properties>(propFile)
+suspend fun main(args: Array<String>) {
+    val token = args.firstOrNull() ?: throw IllegalArgumentException("Missing token")
+    val propFile = Properties::class.java.getResource("/hangoutbot_properties.json").readText()
+    val properties = Json.decodeFromString<Properties>(propFile)
 
-        bot(configuration.token) {
-            inject(configuration)
-            inject(properties)
+    bot(token) {
+        inject(properties)
 
-            prefix {
-                val persistentData = discord.getInjectionObjects(PersistentData::class)
-                guild?.let { persistentData.getGuildProperty(it) { prefix() } } ?: "++"
+        prefix {
+            val persistentData = discord.getInjectionObjects(PersistentData::class)
+            guild?.let { persistentData.getGuildProperty(it) { prefix } } ?: "+"
+        }
+
+        configure {
+            allowMentionPrefix = true
+            requiresGuild = false
+            generateCommandDocs = false
+            commandReaction = null
+            theme = Color.CYAN
+        }
+
+        mentionEmbed {
+            val botStats = it.discord.getInjectionObjects(BotStatsService::class)
+            val channel = it.channel
+            val self = channel.kord.getSelf()
+
+            color = it.discord.configuration.theme
+
+            thumbnail {
+                url = self.avatar.url
             }
 
-            configure {
-                allowMentionPrefix = true
-                requiresGuild = false
-                generateCommandDocs = false
-                commandReaction = null
-                theme = Color.CYAN
+            field {
+                name = self.tag
+                value = "A bot to manage utility commands and functionality that does not warrant its own bot"
             }
 
-            mentionEmbed {
-                val botStats = it.discord.getInjectionObjects(BotStatsService::class)
+            addInlineField("Prefix", it.prefix())
+            addInlineField("Contributors", "markhc#8366")
 
-                val channel = it.channel
-                val self = channel.kord.getSelf()
-                color = it.discord.configuration.theme
+            with(properties) {
+                val kotlinVersion = KotlinVersion.CURRENT
 
-                thumbnail {
-                    url = self.avatar.url
+                field {
+                    name = "Build Info"
+                    value = "```" +
+                        "Version:   $version\n" +
+                        "DiscordKt: $version\n" +
+                        "Kotlin:    $kotlinVersion" +
+                        "```"
                 }
 
                 field {
-                    name = self.tag
-                    value = "A bot to manage utility commands and functionality that does not warrant its own bot"
+                    name = "Uptime"
+                    value = botStats.uptime
                 }
 
-                addInlineField("Prefix", it.prefix())
-                addInlineField("Contributors", "markhc#8366")
-
-                with(properties) {
-                    val kotlinVersion = KotlinVersion.CURRENT
-
-                    field {
-                        name = "Build Info"
-                        value = "```" +
-                            "Version:   $version\n" +
-                            "DiscordKt: $version\n" +
-                            "Kotlin:    $kotlinVersion" +
-                            "```"
-                    }
-
-                    field {
-                        name = "Uptime"
-                        value = botStats.uptime
-                    }
-
-                    field {
-                        name = "Source"
-                        value = "[[GitHub]](${repository})"
-                    }
+                field {
+                    name = "Source"
+                    value = "[[GitHub]](${repository})"
                 }
             }
+        }
 
-            permissions {
-                val permissionsService = discord.getInjectionObjects(PermissionsService::class)
+        permissions {
+            val permissionsService = discord.getInjectionObjects(PermissionsService::class)
 
-                if (guild != null || !(command.requiresGuild ?: discord.configuration.requiresGuild))
-                    permissionsService.isCommandVisible(guild!!, user, command)
-                else
-                    false
-            }
+            if (guild != null || !(command.requiresGuild ?: discord.configuration.requiresGuild))
+                permissionsService.isCommandVisible(guild!!, user, command)
+            else
+                false
         }
     }
 }
--- src/main/kotlin/me/markhc/hangoutbot/dataclasses/BotConfiguration.kt
+++ src/main/kotlin/me/markhc/hangoutbot/dataclasses/BotConfiguration.kt
@@ -1,24 +1,12 @@
 package me.markhc.hangoutbot.dataclasses
 
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
+import kotlinx.serialization.Serializable
 import me.jakejmattson.discordkt.api.dsl.Data
-import java.io.File
 
-data class BotConfiguration(val token: String = "",
-                            var prefix: String = "++",
+data class BotConfiguration(var prefix: String = "++",
                             val ownerId: String = "") : Data("config/config.json")
 
+@Serializable
 data class Properties(val version: String = "",
                       val discordkt: String = "",
                       val repository: String = "")
-
-suspend fun loadConfig(onFinishedLoading: suspend (BotConfiguration?) -> Unit) {
-    val configFile = File("config/config.json")
-
-    if (!configFile.exists()) {
-        return onFinishedLoading(null)
-    }
-
-    return onFinishedLoading(Json.decodeFromString<BotConfiguration>(configFile.readText()))
-}
