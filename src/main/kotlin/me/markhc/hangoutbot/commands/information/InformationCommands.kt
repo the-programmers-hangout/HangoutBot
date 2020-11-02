b4be2b445cf4eaccc204eeadf34e4720052327da
moe <moe9999@protonmail.com>
2020-11-02 11:14:21 +0000

    Fix issue with invites

--- src/main/kotlin/me/markhc/hangoutbot/commands/information/InformationCommands.kt
+++ src/main/kotlin/me/markhc/hangoutbot/commands/information/InformationCommands.kt
@@ -1,9 +1,14 @@
 package me.markhc.hangoutbot.commands.information
 
-import me.jakejmattson.discordkt.api.arguments.*
+import me.jakejmattson.discordkt.api.arguments.CommandArg
+import me.jakejmattson.discordkt.api.arguments.RoleArg
+import me.jakejmattson.discordkt.api.arguments.UserArg
 import me.jakejmattson.discordkt.api.dsl.commands
-import me.markhc.hangoutbot.services.*
-import me.markhc.hangoutbot.utilities.*
+import me.markhc.hangoutbot.services.HelpService
+import me.markhc.hangoutbot.utilities.buildGuildInfoEmbed
+import me.markhc.hangoutbot.utilities.buildMemberInfoEmbed
+import me.markhc.hangoutbot.utilities.buildRoleInfoEmbed
+import me.markhc.hangoutbot.utilities.buildUserInfoEmbed
 
 fun produceInformationCommands(helpService: HelpService) = commands("Information") {
     command("help") {
@@ -22,10 +27,12 @@ fun produceInformationCommands(helpService: HelpService) = commands("Information
     guildCommand("invite") {
         description = "Generates an invite link to this server."
         execute {
+            //TODO this wont be needed after 0.7.0 kord
+            try {
+                val invite = guild.getVanityUrl() ?: throw Exception()
 
-            if (guild.getVanityUrl() != null) {
-                respond(guild.getVanityUrl()!!)
-            } else {
+                respond(invite)
+            } catch (ex: Exception) {
                 val guildChannel = guild.rulesChannel ?: guild.systemChannel ?: guild.publicUpdatesChannel ?: channel
 
                 // TODO: Cache these invites so we don't generate a new one every time
@@ -33,17 +40,29 @@ fun produceInformationCommands(helpService: HelpService) = commands("Information
                     age = 86400
                 }
 
-                respond("Here's your invite! It will expire in 24 hours!\n${invite.code}")
+                respond("Here's your invite! It will expire in 24 hours!\nhttps://discord.gg/${invite.code}")
             }
+
+
+//            if (guild.getVanityUrl() != null) {
+//                respond(guild.getVanityUrl()!!)
+//            } else {
+//                val guildChannel = guild.rulesChannel ?: guild.systemChannel ?: guild.publicUpdatesChannel ?: channel
+//
+//                // TODO: Cache these invites so we don't generate a new one every time
+//                val invite = guildChannel.asChannel().createInvite {
+//                    age = 86400
+//                }
+//
+//                respond("Here's your invite! It will expire in 24 hours!\n${invite.code}")
+//            }
         }
     }
 
     guildCommand("serverinfo") {
         description = "Display a message giving basic server information."
         execute {
-            respond {
-                buildGuildInfoEmbed(guild)
-            }
+            buildGuildInfoEmbed(guild)
         }
     }
 
