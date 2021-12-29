289488acfe9612e42c779fec278481229c36a2bb
Azaria <moe9999@protonmail.com>
2021-12-29 12:38:48 +0000

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,31 @@
-import org.jetbrains.kotlin.config.KotlinCompilerVersion
-
 group = "me.markhc"
 version = Versions.BotVersion
 description = "hangoutbot"
 
 object Versions {
-    const val BotVersion = "3.0.0"
-    const val DiscordKt = "0.21.3"
+    const val BotVersion = "4.0.0"
+
+    const val DiscordKt = "0.23.0-SNAPSHOT"
     const val Fuel = "2.3.1"
-    const val JodaTime = "2.10.10"
     const val Cowsay = "1.1.0"
-    const val Mockk = "1.11.0"
-    const val JUnit = "5.7.0"
 }
 
 plugins {
-    kotlin("jvm") version "1.4.10"
-    kotlin("plugin.serialization") version "1.5.0"
-    id("com.github.ben-manes.versions") version "0.38.0"
-    id("com.github.johnrengelman.shadow") version "7.0.0"
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.serialization") version "1.6.10"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 repositories {
     mavenCentral()
-    jcenter()
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
 }
 
 dependencies {
-    implementation(kotlin("scripting-compiler-embeddable", KotlinCompilerVersion.VERSION))
-    implementation(kotlin("compiler-embeddable", KotlinCompilerVersion.VERSION))
-    implementation(kotlin("script-runtime", KotlinCompilerVersion.VERSION))
-    implementation(kotlin("script-util", KotlinCompilerVersion.VERSION))
-
     implementation("me.jakejmattson:DiscordKt:${Versions.DiscordKt}")
     implementation("com.github.kittinunf.fuel:fuel-gson:${Versions.Fuel}")
     implementation("com.github.kittinunf.fuel:fuel:${Versions.Fuel}")
-    implementation("joda-time:joda-time:${Versions.JodaTime}")
     implementation("com.github.ricksbrown:cowsay:${Versions.Cowsay}")
-
-    testImplementation("io.mockk:mockk:${Versions.Mockk}")
-    testImplementation(platform("org.junit:junit-bom:${Versions.JUnit}"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks {
@@ -48,15 +33,8 @@ tasks {
         kotlinOptions.jvmTarget = "1.8"
     }
 
-    test {
-        useJUnitPlatform()
-        testLogging {
-            events("passed", "skipped", "failed")
-        }
-    }
-
     shadowJar {
-        archiveFileName.set("HangoutBot-${Versions.BotVersion}.jar")
+        archiveFileName.set("HangoutBot.jar")
         manifest {
             attributes(
                 "Main-Class" to "me.markhc.hangoutbot.MainKt"
